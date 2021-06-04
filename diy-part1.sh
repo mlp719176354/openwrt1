8bcce26c85c22c1f64cb667892dd266896c18aa8
mlp719176354 <46795470+mlp719176354@users.noreply.github.com>
2021-06-05 00:56:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/kuling321/inode7.1_for_openwrt.git package/inode7.1_for_openwrt
+git clone https://github.com/kuling321/inode7.1_for_openwrt.git package/inode7.1
+git clone https://github.com/liuqun/njit8021xclient.git package/njit8021xclient
